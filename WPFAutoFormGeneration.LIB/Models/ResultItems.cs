168c369b3267d323aec6cd30fdcc2f1b185ce076
sad_degenerate <mister.franz02@gmail.com>
2023-04-18 14:09:07 +0700

    empty checkboxes and listboxes now don't throw exceptions

--- WPFAutoFormGeneration.LIB/Models/ResultItems.cs
+++ WPFAutoFormGeneration.LIB/Models/ResultItems.cs
@@ -11,7 +11,7 @@ public class ResultItems : BaseItem
     public ResultItems(string controlName, ControlType controlType, List<string> resultsList) 
         : base("result", controlName, controlType, new Style())
     {
-        if (resultsList.Count == 0)
+        if (resultsList.Count == 0 && ControlType != ControlType.CheckBox && ControlType != ControlType.ListBox)
         {
             throw new ArgumentNullException(nameof(resultsList), "Results list can't be empty.");
         }
